302cd639556d3a52c944060a3059daacec392825
SrikarAppani <appanisrikar2019@gmail.com>
2025-05-21 18:19:54 +0530

    Resolved the React import error

--- frontend/src/pages/RoleSelectionPage.jsx
+++ frontend/src/pages/RoleSelectionPage.jsx
@@ -1,4 +1,5 @@
-import React, { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from 'react-router-dom';
 import '../styles/RoleSelectionPage.css';
 
 const RoleSelectionPage = () => {
